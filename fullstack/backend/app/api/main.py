e216038eedda0e31c693cb4813fc7940a4868821
igiemanlangit <igie.manlangit@yahoo.com.au>
2026-05-12 13:53:11 +0930

    Add project subtask assignment models, migration, and PATCH endpoint. Implement subtask workforce assignment endpoint and permissions

--- fullstack/backend/app/api/main.py
+++ fullstack/backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
  
-from app.api.routes import login, projects, users, utils, statuses
+from app.api.routes import login, projects, users, utils, statuses, project_subtasks
 from app.core.config import settings
 from app.api.routes import workforce_allocate
  
@@ -11,7 +11,7 @@ api_router.include_router(projects.router)
 api_router.include_router(utils.router)
 api_router.include_router(statuses.router)
 api_router.include_router(workforce_allocate.router)
- 
+api_router.include_router(project_subtasks.router)
  
 # if settings.ENVIRONMENT == "local":
 #     api_router.include_router(private.router)
